99fc29ef6a2602605b5ecb7ce40da15aeda9a271
eric31512 <eric31512@gmail.com>
2021-06-19 21:30:14 +0800

    final test 1 is over

---
+++ cmake-build-debug/CMakeFiles/untitled5hw2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5hw2.dir/hw2.cpp.obj"
+  "libuntitled5hw2.dll.a"
+  "untitled5hw2.exe"
+  "untitled5hw2.exe.manifest"
+  "untitled5hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled5hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
